321a7db54278fe318719fbab2dd9e3978b8d5c51
myohmai <mai.xxx.mode.08@gmail.com>
2026-06-25 19:10:07 +0900

    set: openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getLocale } from "next-intl/server";
 import { GoogleAnalytics } from "@next/third-parties/google";
@@ -7,6 +8,31 @@ import { ThemeInitializer } from "@/app/components/ThemeInitializer";
 import 'destyle.css'
 import './layout.scss';
 import "@/app/styles/globals.scss";
+
+export const metadata: Metadata = {
+    title: "TangledTo",
+    description: "Topic Based Social Media",
+    openGraph: {
+        title: "TangledTo",
+        description: "Topic Based Social Media",
+        url: "https://www.tangledto.com", 
+        siteName: "tanto",
+        images: [
+            {
+                url: "/images/202600620.png", 
+                width: 1200,
+                height: 630,
+            },
+        ],
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "TangledTo",
+        description: "Topic Based Social Media",
+        images: ["/images/202600620.png"],
+    },
+};
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
     const locale = await getLocale();
